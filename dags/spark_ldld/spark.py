bd04f7783dfe3579cd134a2ccec25a785c16803c
kristenbwin2 <duong.nguyen.dang@vdigital.vn>
2022-12-14 17:25:03 +0700

    dg add

--- dags/spark_ldld/spark.py
+++ dags/spark_ldld/spark.py
@@ -1,9 +1,10 @@
 import os
 import pyspark
 from pyspark.sql import SparkSession
+from sqlalchemy import create_engine
 from textwrap import dedent
-from pyspark.sql.functions import *
-from delta.tables import *
+from airflow.providers.oracle.hooks.oracle import OracleHook
+import pyspark.sql.functions as f
 os.environ["PYSPARK_SUBMIT_ARGS"] = "--driver-class-path ojdbc7-12.1.0.2.jar --jars ojdbc7-12.1.0.2.jar --master local[3] pyspark-shell"
 
 appName = "PySpark Example - Oracle Example"
@@ -26,14 +27,21 @@ service_name = 'oracle19db'
 jdbcUrl = f"jdbc:oracle:thin:@{server}:{port}:{service_name}"
 jdbcDriver = "oracle.jdbc.driver.OracleDriver"
 
+conn = OracleHook.get_connection('oracle_dwh')
+engine = create_engine(
+    f'oracle+cx_oracle://{conn.login}:{conn.password}@{conn.host}:{conn.port}/{conn.schema}')
 
-df_stg = spark.read.format("jdbc") \
+def read_df(query):
+    return spark.read.format("jdbc") \
     .option("url", jdbcUrl) \
-    .option("query", sql_select_stg) \
+    .option("query", query) \
     .option("user", user) \
     .option("password", password) \
     .option("driver", jdbcDriver) \
     .load()
+    
+
+df_stg = read_df(sql_select_stg)
     
 df_dim = spark.read.format("jdbc") \
     .option("url", jdbcUrl) \
@@ -52,12 +60,6 @@ df_readfull_dim = spark.read.format("jdbc") \
     .load()
 
 # df_stg.show()
-# print(df_dim.rdd.isEmpty())
-table_name = "delete_tab_row"
-df_readfull_dim.write.saveAsTable(table_name)
-
-# deltaTable = DeltaTable.
-
 
 if df_dim.rdd.isEmpty():
     df_stg.write.format('jdbc').options(
@@ -68,10 +70,24 @@ if df_dim.rdd.isEmpty():
         password='Vdc2022').mode('append').save()
 else:
     tempdf = df_stg.subtract(df_dim)
-    tempdf.show()
-    # tempdf.write.format('jdbc').options(
-    #     url=f"jdbc:oracle:thin:@{server}:{port}:{service_name}",
-    #     driver='oracle.jdbc.driver.OracleDriver',
-    #     dbtable='DWH_DIM.DIM_LDLD_CHUC_VU_LIEN_DOAN1',
-    #     user='ODI_REPO',
-    #     password='Vdc2022').mode('overwrite').save()
+    # tempdf.show()
+    # print(tempdf.rdd.collect()[0])
+    list_filter = tempdf.rdd.map(lambda x: x['MA_CHUC_VU']).collect()
+    list_value = tempdf.rdd.map(lambda x: x['TEN_CHUC_VU']).collect()
+    # df1 = df_readfull_dim.filter(df_readfull_dim.MA_CHUC_VU.isin(list_filter))['MA_CHUC_VU', 'TEN_CHUC_VU']
+    # df1.show()
+
+    sql = """ BEGIN 
+                UPDATE "DWH_DIM"."DIM_LDLD_CHUC_VU_LIEN_DOAN1"
+                SET "TEN_CHUC_VU" = '%s'
+                WHERE "MA_CHUC_VU" = '%s';
+                END;
+                """%(list_filter[0], list_value[0])
+    print(sql)
+    try:
+        with engine.begin() as conn:
+            conn.execute(sql)
+            print("success insert")
+    except:
+        raise Exception("Fail insert")
+
